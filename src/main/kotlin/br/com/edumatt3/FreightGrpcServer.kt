548223f964fbd05f5f3a58dc121286e9b5a34f85
eduardo-zup <eduardo.bento@zup.com.br>
2021-08-15 13:39:49 -0300

    add errors

--- src/main/kotlin/br/com/edumatt3/FreightGrpcServer.kt
+++ src/main/kotlin/br/com/edumatt3/FreightGrpcServer.kt
@@ -1,7 +1,13 @@
 package br.com.edumatt3
 
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
 import javax.inject.Singleton
 import kotlin.random.Random
 
@@ -16,9 +22,57 @@ class FreightGrpcServer : FreightServiceGrpc.FreightServiceImplBase() {
     ) {
         logger.info("Calculating freight for request: $request")
 
+        val zipcode = request?.zipcode
+        if (zipcode.isNullOrBlank()){
+            val exception = Status.INVALID_ARGUMENT
+                .withDescription("zipcode should be present")
+                .asRuntimeException()
+
+            responseObserver?.onError(exception)
+        }
+
+        if (!zipcode!!.matches("[0-9]{5}-[0-9]{3}".toRegex())){
+            val exception = Status.INVALID_ARGUMENT
+                .withDescription("invalid zipcode")
+                .augmentDescription("expected format should be 99999-99")
+                .asRuntimeException()
+
+            responseObserver?.onError(exception)
+        }
+
+        //SIMULAR verificação de seg
+        //o status de erro acima não permite objetos complexos, esse novo do google sim
+        //o cliente deve estar ciente desses detalhes
+        if (zipcode.endsWith("333")){
+            val statusProto = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED.number)
+                .setMessage("User cant access this feature ")
+                .addDetails(Any.pack(ErrorDetails.newBuilder().setCode(401).setMessage("token expirado").build())) // precisa definir essa mensagem de erro no protobuf
+                .build()
+
+            //convert para statusruntimeexcep..
+
+            val exception = StatusProto.toStatusRuntimeException(statusProto)
+            responseObserver?.onError(exception)
+        }
+
+
+        var value = 0.0
+
+        //tratar erros
+        try {
+            value = Random.nextDouble(from = 0.0, until = 140.0)//complexy logic
+            //simula erro
+            if(value > 100.0) throw IllegalArgumentException("Erro inesperado ao executar lógica de negócio!")
+        } catch (e: Exception) {
+            responseObserver?.onError(Status.INTERNAL
+                .withDescription(e.message)
+                .withCause(e)//anexado ao status, mas não é enviado ao Client
+                .asRuntimeException())
+        }
         val response = CalculateFreightResponse.newBuilder()
             .setZipcode(request!!.zipcode)
-            .setValue(Random.nextDouble(from = 0.0, until = 140.0))
+            .setValue(value)
             .build()
 
         logger.info("Calculated freight: $response")
